79b5fa267cbac2f98d0531b29bd0253f6d56079c
HasanNaser <19hasan0@gmail.com>
2021-06-26 15:55:04 +0300

    add age informatios

--- js/script.js
+++ js/script.js
@@ -33,7 +33,8 @@ stopbtn.addEventListener("mouseover", function () {
   this.style.top = getRndInteger(-10, 99) + "%";
   BODY.style.background = getRandomColor();
   this.innerHTML = "Stop it :)";
-  this.style.bottom = "auto"; 
+  this.style.bottom = "auto";
+  this.style.background = getRandomColor();
 });
 
 stopbtn.addEventListener("click", function () {
@@ -58,3 +59,32 @@ function getRandomColor() {
 document.querySelector("#date_").innerHTML =(new Date()).getFullYear();
 // document.querySelector(".footer").style.marginTop = (window.innerHeight/2) - 250 +"px";
 // console.log(window.innerHeight)
+
+
+function pad(d) {
+  return (d < 10) ? '0' + d.toString() : d.toString();
+}
+
+
+var calculateAge = ()=>{
+  let age_year = document.querySelector('#meInTheWorld .years');
+  let age_months = document.querySelector('#meInTheWorld .months');
+  let age_days = document.querySelector('#meInTheWorld .days');
+  let age_hours = document.querySelector('#meInTheWorld .hours');
+  let age_minutes = document.querySelector('#meInTheWorld .minutes');
+  let age_seconds = document.querySelector('#meInTheWorld .seconds');
+  let birth_date = new Date('Jan, 05, 1997');
+  let years,months,days, hours, minutes, seconds;
+  setInterval(function(){
+    var current_date = new Date();
+    age_year.innerHTML = pad(current_date.getYear() - birth_date.getYear());
+    age_months.innerHTML = pad(current_date.getMonth() - birth_date.getMonth());
+    age_days.innerHTML = pad(current_date.getDate() - birth_date.getDate());
+    age_hours.innerHTML = pad(current_date.getHours() - birth_date.getHours());
+    age_minutes.innerHTML = pad(current_date.getMinutes() - birth_date.getMinutes());
+    age_seconds.innerHTML = pad(current_date.getSeconds() - birth_date.getSeconds());
+  },1000);
+
+}
+
+calculateAge();
